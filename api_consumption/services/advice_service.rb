08e122334c1e998e3ef3c82787e933aa5d2b369b
Colin Alexander <colintalex@gmail.com>
2020-08-14 01:13:27 -0600

    Adds random and search by term endpoints. Tests passing.

--- api_consumption/services/advice_service.rb
+++ api_consumption/services/advice_service.rb
@@ -1,3 +1,21 @@
+require 'faraday'
+require 'json'
+
 class AdviceService
-    
+
+    def random
+      response = connection.get('/advice')
+      JSON.parse(response.body)
+    end
+
+    def search(term)
+      response = connection.get("/advice/search/#{term}")
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def connection
+      Faraday.new 'https://api.adviceslip.com'
+    end
 end
